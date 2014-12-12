0d8b23da3a3ac374158fca3a14349bb29b3c0897
Emelie Ertan <emelie.ertan@fysik.su.se>
2014-12-12 13:56:49 +0100

    separate flags for geometry input and disp. input

--- main.f90
+++ main.f90
@@ -13,35 +13,41 @@ program main
     real(rk), dimension(1:maxmodes,1:step) :: r1, r2, a
     real(rk), dimension(1:maxmodes,1:maxatoms,1:3,1:step) :: xyz 
     character(len=5) :: geo='GEOME', disp='DISPL', inte='INTER'   
-    integer :: flag
+    integer :: hflag, iflag
 
-    flag = read_key(geo)
+    hflag = read_key(geo)
 
-    if(flag==1) then
+    if(hflag==1) then
         call init_geo(xyz0)
         
         call init_internal_geo(xyz0, r01, r02, a0)
-    end if
 
-    flag = read_key(disp)    
-    if(flag==1) then
-        call read_cartd(dxyz)
+        iflag = read_key(disp)    
         
-        call make_cartesian(xyz, xyz0, dxyz)
+        if(iflag==1) then
         
-        call write_cartesian(xyz) 
-    end if
+            call read_cartd(dxyz)
+        
+            call make_cartesian(xyz, xyz0, dxyz)
+        
+            call write_cartesian(xyz) 
+        
+        end if
 
-    flag = read_key(inte)
+        iflag = read_key(inte)
     
-    if(flag==1) then
-        call read_interd(drtheta)
+        if(iflag==1) then
+        
+            call read_interd(drtheta)
 
-        call make_internal(r01, r02, a0, r1, r2, a, drtheta)
+            call make_internal(r01, r02, a0, r1, r2, a, drtheta)
 
-        call write_internal(r1,r2,a)
+            call write_internal(r1,r2,a)
+        
+        end if
+
+        call write_grid
+
     end if
-
-    call write_grid
 
 end program main
